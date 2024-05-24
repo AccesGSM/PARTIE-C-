2035645551379488c7f8118b2f816cb7e750e301
AccesGSM <accesgsm64@gmail.com>
2024-05-24 13:18:19 +0200

    acces.cpp

--- acces.cpp
+++ acces.cpp
@@ -29,9 +29,7 @@ CPeriode CAcces::GetPeriodeServeur(String& reponse_serveur)
   uint16_t jour_fin = fin.substring(0,2).toInt();
   uint16_t mois_fin = fin.substring(3,5).toInt();
   uint16_t an_fin = fin.substring(6,8).toInt(); 
-//Serial.print("deb : ");
-//Serial.println(periode_reserv.GetMessage());
-	periode_reserv.SetFin(jour_fin, mois_fin, an_fin);
+  periode_reserv.SetFin(jour_fin, mois_fin, an_fin);
   
   return periode_reserv;
 
@@ -40,16 +38,17 @@ CPeriode CAcces::GetPeriodeServeur(String& reponse_serveur)
 // Fonction pour mettre à jour la date et l'heure de l'instant
 void CAcces::SetHeureSystem(String& reponse_serveur) 
 {
-	//"date: Thu, 31 Aug 2023 10:47:23 GMT"
-    // Extraire la date et l'heure de la réponse du serveur
+//"date: Thu, 31 Aug 2023 10:47:23 GMT"
+// Extraire la date et l'heure de la réponse du serveur
     int debut_date = reponse_serveur.indexOf("date: ") + 6;
     int fin_date = reponse_serveur.indexOf(" GMT");
     String date_heure = reponse_serveur.substring(debut_date, fin_date);
-    // Extraire les parties de la date et de l'heure
+	
+// Extraire les parties de la date et de l'heure
     String st_jour = date_heure.substring(5, 7);
     unsigned int jour = st_jour.toInt();
     String st_mois = date_heure.substring(8, 11);
-	unsigned int mois = m_horloge.Month2Nombre(st_mois);    
+    unsigned int mois = m_horloge.Month2Nombre(st_mois);    
 
     unsigned int an = date_heure.substring(12, 16).toInt();
     unsigned int heure = date_heure.substring(17, 19).toInt();
@@ -70,39 +69,22 @@ CInstant CAcces::GetHeureSystem()
 	return m_horloge;//+ (millis() - m_millis_start)/1000;
 }
 
-/*CTRLtemps restant
-elle recoit la periode de reservation c_periode
-apres elle calcul le temps restant avec la focntion minute restant par rapport à l'heure systeme
-si il reste des minutes elle appelle la fonction ouvre pour se nombre de minutes restantes
-a la fin elle return les minutes restantes*/
 int CAcces::CtrlTempsRestant(CPeriode reservation)
 {
 	int min_rest = reservation.MinutesRestantes(GetHeureSystem());
-	/*Serial.print("mins restantes :");
-	Serial.println(min_rest);
-	Serial.print("heure sys :");
-	Serial.println(GetHeureSystem().GetMessage());
-	*/
+/*
+Serial.print("mins restantes :");
+Serial.println(min_rest);
+Serial.print("heure sys :");
+Serial.println(GetHeureSystem().GetMessage());
+*/
 	if (min_rest)		Ouvre(min_rest);
 
 	return min_rest;
-	
 }
 
-/*ouvre elle renvois rien, elle recoit des minutes d'ouverture 
-si l'acces est hors service ou so les minutes sont = a 0 
-l'instant de fermeture (l'instant ferm = 0)
-sinon
-l'instant de fermeture prends l'heure systeme
-controle durée gache 
-si il existent de seconde gache 
-l'instant ferme + seconde gache
-autrtement c'est une ouverture public 
-l'instant ferme + les minutes d'ouverture ens econdes
-on fais une sauvegarde en EPROM et ensuite on controle la commande avce la fonction commande*/
 void CAcces::Ouvre(int minutes)
 {
-	//minutes = m_sec_gache;
 	if (m_hs == false || minutes == 0)
 		m_instant_ferme.SetInstant(0);
 	else
@@ -113,8 +95,6 @@ void CAcces::Ouvre(int minutes)
 	else
 		m_instant_ferme.Add(minutes*60);
 	
-	//on fais une sauvegarde en EPROM
-	//et ensuite on controle la commande avce la fonction commande
 }
 
 void CAcces::Controle()
